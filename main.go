bd263c40b91f60fe2ee4fb388fddf359720224d1
Baturax <bai@baturax.com>
2025-07-21 10:15:07 +0300

    something in the way

--- main.go
+++ main.go
@@ -1,14 +1,79 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
+var config Config
+
 func main() {
-	tokenCheck()
+	configFile()
+	translate()
+}
+
+func translate() {
+	apiKey := tokenCheck()
+	url := "https://api-free.deepl.com/v2/translate"
+
+	requestBody := map[string]any{
+		"text":        []string{getInput()},
+		"target_lang": config.TargetLanguage,
+	}
+	jsonData, err := json.Marshal(requestBody)
+	if err != nil {
+		fmt.Println("JSON encode error:", err)
+		return
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	Err(err)
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "DeepL-Auth-Key "+apiKey)
+
+	resp, err := http.DefaultClient.Do(req)
+	Err(err)
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	Err(err)
+
+	fmt.Println("Response:")
+	fmt.Println(string(body))
+}
+
+func getInput() string {
+
+	args := os.Args[1:]
+	return strings.Join(args, " ")
+}
+
+type Config struct {
+	TargetLanguage string `json:"target_language"`
+}
+
+func configFile() string {
+	home := os.Getenv("HOME")
+	configPath := filepath.Join(home, ".config", "deepl-translator", "config.json")
+	jsonFile, err := os.Open(configPath)
+	Err(err)
+
+	defer jsonFile.Close()
+
+	fl, err := io.ReadAll(jsonFile)
+	Err(err)
+	json.Unmarshal(fl, &config)
+
+	return config.TargetLanguage
+
 }
 
 func tokenCheck() string {
